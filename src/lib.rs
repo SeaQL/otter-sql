438c17092ecb6f83d47e319ba691f686768e7dc8
Samyak S Sarnayak <samyak201@gmail.com>
2022-06-14 23:44:41 +0530

    feat: setup Rust project and directory structure

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod database;
+mod schema;
+mod table;
+mod vm;
+mod ic;
+mod execute;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ src/schema.rs
@@ -0,0 +1,11 @@
+struct Schema {}
+
+#[cfg(test)]
+mod tests {
+    use super::Schema;
+
+    #[test]
+    fn create_schema() {
+        let _ = Schema{};
+    }
+}
