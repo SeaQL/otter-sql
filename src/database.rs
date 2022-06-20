e8415daa1313f5d18c146424e31763f0606b8095
Samyak S Sarnayak <samyak201@gmail.com>
2022-06-20 23:41:20 +0530

    wip: data structures (this commit will be squashed)

--- src/database.rs
+++ src/database.rs
@@ -3,22 +3,30 @@ use crate::schema::Schema;
 const DEFAULT_SCHEMA_NAME: &str = "main";
 
 pub struct Database {
-    pub name: String,
-    pub schemas: Vec<Schema>,
+    name_: String,
+    schemas_: Vec<Schema>,
 }
 
 impl Database {
-    pub fn new(name: String) -> Self
+    pub fn new(name_: String) -> Self
     {
         Self {
-            name,
-            schemas: vec![Schema::new(DEFAULT_SCHEMA_NAME.to_owned())],
+            name_,
+            schemas_: vec![Schema::new(DEFAULT_SCHEMA_NAME.to_owned())],
         }
     }
 
     pub fn with_schema(mut self, schema: Schema) -> Self {
-        self.schemas.push(schema);
+        self.schemas_.push(schema);
         self
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name_
+    }
+
+    pub fn schemas(&self) -> &Vec<Schema> {
+        &self.schemas_
     }
 }
 
