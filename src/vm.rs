9d91ece5dc4c94041cb71cde77bf2d570bf939b4
Samyak S Sarnayak <samyak201@gmail.com>
2022-06-20 23:20:09 +0530

    wip: data structures (this commit will be squashed)

--- src/vm.rs
+++ src/vm.rs
@@ -1,11 +1,39 @@
 use std::collections::HashMap;
+use std::fmt::Display;
 
 use crate::Mrc;
 use crate::{ic::IntermediateCode, table::Table};
 
+/// An index that can be used to access a specific register.
+#[derive(Default, Clone, PartialEq, Eq, Hash)]
+pub struct RegisterIndex(usize);
+
+impl RegisterIndex {
+    /// Get the next index in the sequence.
+    fn next_index(&self) -> RegisterIndex {
+        RegisterIndex(self.0 + 1)
+    }
+}
+
+impl Display for RegisterIndex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "%{}", self.0)
+    }
+}
+
 #[derive(Default)]
 pub struct VirtualMachine {
-    pub registers: HashMap<usize, Register>,
+    registers: HashMap<RegisterIndex, Register>,
+    last_index: RegisterIndex,
+}
+
+impl VirtualMachine {
+    pub fn insert(&mut self, reg: Register) -> RegisterIndex {
+        let index = self.last_index.next_index();
+        self.registers.insert(index.clone(), reg);
+        self.last_index = index.clone();
+        index
+    }
 }
 
 impl VirtualMachine {
